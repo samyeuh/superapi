50476237addd008150acc3b6c947d59e039f82e6
samyeuh <samybouhamidi2@gmail.com>
2024-12-12 19:29:35 +0100

    remove getScoreboard

--- src/main/java/com/samy/api/SuperAPI.java
+++ src/main/java/com/samy/api/SuperAPI.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public interface SuperServeurAPI {
+public interface SuperAPI {
     Map<UUID, List<UUID>> getFriends();
     void createRankTab();
     void createTeams(List<TeamGame> teams);
     void joinTeam(Player player, TeamGame team);
+
+    void setScoreboard(String header, List<String> lines, Player player);
 }
